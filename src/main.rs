b61717879267604c9660cc4a8d0dcf0ee7ab7d08
ventus_orientis <simon.kraemer@outlook.de>
2020-06-10 20:35:16 +0200

    working minimal version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+use std::io::{stdin, stdout, Write};
+use std::path::Path;
+use std::process::{Child, Command, Stdio};
+
 fn main() {
-    println!("Hello, world!");
+    loop{
+        print!("sushi:");
+        stdout().flush();
+
+        let mut user_eingabe = String::new();
+        stdin().read_line(&mut user_eingabe).unwrap();
+
+        //new-line symbol aus stdin entfernen
+        let befehl = user_eingabe.trim();
+
+        let mut child = Command::new("cmd")
+            .args(&["/C", befehl])
+            .spawn()
+            .unwrap();
+
+        child.wait();
+    }
 }
